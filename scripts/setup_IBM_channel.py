4ab1d2e33de7fda07464b51ad1d14f515afa2565
Kaleb Coberly <53658348+KalebCoberly@users.noreply.github.com>
2025-03-04 08:52:51 -0800

    Setup/kaleb coberly/add requirements setup etc (#1)
    Adds requirements for hello world, setup instructions and script, hello world notebook.

---
+++ scripts/setup_IBM_channel.py
@@ -0,0 +1,67 @@
+"""Set up the IBM Quantum Platform channel for Qiskit Runtime Service.
+
+Follows along to https://docs.quantum.ibm.com/guides/setup-channel
+"""
+
+import os
+
+from dotenv import load_dotenv
+import logging
+
+from qiskit import QuantumCircuit
+from qiskit_ibm_runtime import QiskitRuntimeService, SamplerV2 as Sampler
+
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
+
+
+def main() -> None:
+    QiskitRuntimeService.save_account(
+        token=get_ibm_quantum_token(),
+        channel="ibm_quantum",  # IBM Quantum Platform channel (vs. IBM Cloud platform).
+        set_as_default=True,
+    )
+    print("IBM Quantum Platform channel set up successfully. Testing service ...")
+    test_service()
+
+    return
+
+
+def get_ibm_quantum_token() -> str:
+    """Get the IBM Quantum API token."""
+    load_dotenv()
+    key = os.getenv("IBM_QUANTUM_API_TOKEN")
+    if not key:
+        raise ValueError(
+            "IBM_QUANTUM_API_TOKEN not found. Set the environment variable."
+        )
+
+    return key
+
+def test_service() -> None:
+    """Test the Qiskit Runtime Service."""
+    # Spent 2 hours and 40 minutes in queue.
+    # Estimated quantum runtime: 3.4s.
+    # Actual: 9s.
+    example_circuit = QuantumCircuit(2)
+    example_circuit.measure_all()
+    
+    # Now that the account is saved, we can use the Qiskit Runtime without specifying further authentication.
+    service = QiskitRuntimeService()
+    backend = service.least_busy(operational=True, simulator=False)
+    
+    sampler = Sampler(backend)
+
+    logger.info("Running the circuit on the backend ...")
+    job = sampler.run([example_circuit])
+    logger.info(f"job id: {job.job_id()}")
+
+    logger.info("Getting the result ...")
+    result = job.result()
+    logger.info(result)
+
+    return
+ 
+
+if __name__ == "__main__":
+    main()
